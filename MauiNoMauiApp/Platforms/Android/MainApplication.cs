792fa905c935f2671bc4309728d8a846035e13f2
杨 周 <1570571499@qq.com>
2022-03-03 21:57:03 +0800

    Add project files.

---
+++ MauiNoMauiApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Runtime;
+using System;
+
+namespace MauiNoMauiApp
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+    }
+}
